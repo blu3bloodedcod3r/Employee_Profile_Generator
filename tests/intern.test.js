4436f67ea2a0cfe56f8c9288fa3c34f0640289e9
Crystal Aguilar <crystal_0688@yahoo.com>
2022-10-21 21:25:02 -0700

    intern test additions

---
+++ tests/intern.test.js
@@ -0,0 +1,11 @@
+const Employee = require('../lib/Employee')
+const Intern = require('../lib/intern')
+
+test('Should create an instance of class Emplyee', () => {
+    //arrange
+
+    //act
+
+    //assert
+    
+})

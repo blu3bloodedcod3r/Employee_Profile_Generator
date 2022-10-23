738a0fc6bf5a4a5041a533f4673cc1624a0b6b7d
Crystal Aguilar <crystal_0688@yahoo.com>
2022-10-23 11:03:53 -0700

    engineer file added

--- tests/engineer.test.js
+++ tests/engineer.test.js
@@ -1,10 +1,31 @@
 const Employee = require('../lib/Employee.js');
+const Engineer = require('../lib/Engineer')
 
-test('Should create an instance of class Employee', () => {
-    //arrange
+const engineerInfo = new Engineer('Tommy Pickles', '7230', 'topic@email.com', 'githubName@github.com');
 
-    //act
-    const newEmployee = new Employee();
-    //assert
-    expect(typeof newEmployee).toBe('object')
-})
+test('test  if able to grab constructor values for object: engineerInfo', () => {
+    expect(engineerInfo.name).toBe('Tommy Pickles');
+    expect(engineerInfo.id).toBe('1026');
+    expect(engineerInfo.email).toBe('topic@email.com');
+    expect(engineerInfo.getGithub).toBe('githubName@github.com');
+});
+
+test('test if able to grab name from getName()', () => {
+    expect(engineerInfo.getName()).toBe('Tommy Pickles');
+});
+
+test('test if able to grab name from getId()', () => {
+    expect(engineerInfo.getId()).toBe('1026');
+});
+
+test('test if able to grab name from getEmail()', () => {
+    expect(engineerInfo.getEmail()).toBe('topic@email.com');
+});
+
+test('test if able to grab name from getGithub()', () => {
+    expect(engineerInfo.getGithub()).toBe('githubName@github.com');
+});
+
+test('test if able to grab name from getRole()', () => {
+    expect(engineerInfo.getRole()).toBe('Engineer');
+});

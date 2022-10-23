a6f2954d74cee9ee4b1df00aff1697fb94322aa3
Crystal Aguilar <crystal_0688@yahoo.com>
2022-10-23 10:53:59 -0700

    interntest file added

--- tests/intern.test.js
+++ tests/intern.test.js
@@ -1,11 +1,31 @@
 const Employee = require('../lib/Employee')
 const Intern = require('../lib/intern')
 
-test('Should create an instance of class Emplyee', () => {
-    //arrange
+const internInfo = new Intern('Gracie Richards', '1026', 'grric@email.com', '12');
 
-    //act
+test('test  if able to grab constructor values for object: internInfo', () => {
+    expect(internInfo.name).toBe('Gracie Richards');
+    expect(internInfo.id).toBe('1026');
+    expect(internInfo.email).toBe('grric@email.com');
+    expect(internInfo.school).toBe('UCLA');
+});
 
-    //assert
-    
-})
+test('test if able to grab name from getName()', () => {
+    expect(internInfo.getName()).toBe('Gracie Richards');
+});
+
+test('test if able to grab name from getId()', () => {
+    expect(internInfo.getId()).toBe('1026');
+});
+
+test('test if able to grab name from getEmail()', () => {
+    expect(internInfo.getEmail()).toBe('grric@email.com');
+});
+
+test('test if able to grab name from getofficeNumber()', () => {
+    expect(internInfo.school()).toBe('UCLA');
+});
+
+test('test if able to grab name from getRole()', () => {
+    expect(internInfo.getRole()).toBe('Intern');
+});
